8d8e3a2f241d20e2536e889e90d0c52765b19681
Elena <lentosik75@gmail.com>
2025-11-21 16:47:05 +0200

    Add base for FinanceManager

--- CPP_OOP/FinanceManager.h
+++ CPP_OOP/FinanceManager.h
@@ -1,5 +1,25 @@
 ﻿#pragma once
-class FinanceManager
-{
+#include <vector>
+#include <string>
+#include "Wallet.h"
+#include "Transaction.h"
+#include "Category.h"
+
+class FinanceManager {
+private:
+    std::vector<Wallet*> wallets;
+    std::vector<Transaction> transactions;
+    std::vector<Category> categories;
+
+public:
+    void addWallet(Wallet* wallet);
+    void addTransaction(const Transaction& t);
+
+    Wallet* findWalletByName(const std::string& name);
+    Category* findOrCreateCategory(const std::string& name);
+
+    const std::vector<Transaction>& getTransactions() const;
+    const std::vector<Category>& getCategories() const;
+    const std::vector<Wallet*>& getWallets() const;
 };
 

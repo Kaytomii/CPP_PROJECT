4a491af858dffec9d7cdff8a368b0185e880755d
Elena <lentosik75@gmail.com>
2025-11-21 16:39:10 +0200

    Add base for Wallet

--- CPP_OOP/Wallet.h
+++ CPP_OOP/Wallet.h
@@ -1,5 +1,19 @@
 ﻿#pragma once
-class Wallet
-{
-};
+#include <string>
 
+class Wallet {
+private:
+    std::string name;
+    std::string type;
+    double balance;
+
+public:
+    Wallet(const std::string& name, const std::string& type, double balance = 0.0);
+
+    void addMoney(double sum);
+    bool spendMoney(double sum);
+
+    double getBalance() const;
+    std::string getName() const;
+    std::string getType() const;
+};
